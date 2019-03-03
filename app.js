74d64b4dfec2d87101b02de7927d50fece1389af
khendrikse <karinhendrikse@Karins-Air-2.lan>
2019-03-03 16:22:18 +0100

    Cleaned a bit

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 require('dotenv').config()
-const express = require('express');
-const app = express();
-const opn = require('opn');
-const axios = require('axios');
+const express = require('express')
+const app = express()
+const opn = require('opn')
+const axios = require('axios')
 
+// We're not gonna mutate these
+const callBackUrl = 'http://localhost:3001/callback'
 const clientID = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
 const slackToken = process.env.SLACK_TOKEN
 
-let token = ''
-let response = {}
-let auth_code = ''
+// We're totally gonna mutate these
+let userToken = ''
+let spotifyAccessToken = ''
 let timeRemaining = 1000
 let currentSong = ''
 
-const getToken = (res) => {
-  response = res.json()
-  token = response.socket._httpMessage.req.url.split('=')[1]
-}
+const getUserToken = (res) => userToken = res.json().socket._httpMessage.req.url.split('=')[1]
 
-const getAuth = (token) => {
+const getSpotifyAuth = (userToken) => {
   axios({
     method: 'post',
     url: 'https://accounts.spotify.com/api/token',
     params: {
       grant_type: 'authorization_code',
-      code: token,
-      redirect_uri: 'http://localhost:3001/callback'
+      code: userToken,
+      redirect_uri: callBackUrl
     },
     headers: {
       Authorization: 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64')),
     }
   })
     .then(function (response) {
-      auth_code = response.data.access_token
+      spotifyAccessToken = response.data.access_token
       timerCheck()
     })
     .catch(error => console.error(error))
@@ -42,14 +41,14 @@ const getAuth = (token) => {
 const getCurrentlyPlaying = () => {
   axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
     headers: {
-      Authorization: `Bearer ${auth_code}`
+      Authorization: `Bearer ${spotifyAccessToken}`
     }
   })
     .then(function (response) {
       timeRemaining = (response.data.item.duration_ms - response.data.progress_ms) + 1000
       currentSong = response.data.item.artists[0].name + ' - ' + response.data.item.name
       console.log('Timeout is:', timeRemaining, 'Song is: ', currentSong)
-      setStatus(currentSong)
+      setSlackStatus(currentSong)
     })
     .catch(error => console.error(error))
 }
@@ -59,7 +58,7 @@ const timerCheck = () => {
   setTimeout(timerCheck, timeRemaining)
 }
 
-const setStatus = (currentSong) => {
+const setSlackStatus = (currentSong) => {
   axios.post('https://slack.com/api/users.profile.set', {
     profile: {
       'status_text': `${currentSong}`,
@@ -73,15 +72,15 @@ const setStatus = (currentSong) => {
   ).catch(error => console.error(error))
 }
 
-opn(`https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=code&redirect_uri=http://localhost:3001/callback&scope=user-read-currently-playing%20user-read-playback-state`)
+opn(`https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=code&redirect_uri=${callBackUrl}&scope=user-read-currently-playing%20user-read-playback-state`)
 
 app.get('/callback', function (req, res) {
   res.send('You can now close this window 👋!')
-  getToken(res)
-  getAuth(token)
+  getUserToken(res)
+  getSpotifyAuth(userToken)
 })
 
-// what port to run server on
+// What port to run server on
 app.listen(3001, function () {
-  console.log('server started on port 3001');
-});
+  console.log('server started on port 3001')
+})
